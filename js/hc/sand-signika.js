be14e9b8811ea24a32e70f35ba2f095ddda1a058
himasnhusharma <himanshu.sharma@oodlestechnologies.com>
2016-08-04 15:28:43 +0530

    color schee of graph

--- js/hc/sand-signika.js
+++ js/hc/sand-signika.js
@@ -30,25 +30,25 @@ Highcharts.theme = {
 		colors: [{
             linearGradient: perShapeGradient,
             stops: [
-                [0, 'rgb(247, 111, 111)'],
-                [1, 'rgb(220, 54, 54)']
+            	[0, '#1B2631'],
+                [1, '#3C546D']
                 ]
             }, {
             linearGradient: perShapeGradient,
             stops: [
-                [0, 'rgb(120, 202, 248)'],
-                [1, 'rgb(46, 150, 208)']
+                [0, '#0D293F'],
+                [1, '#1D6195']
                 ]
             }, {
             linearGradient: perShapeGradient,
             stops: [
-                [0, 'rgb(136, 219, 5)'],
-                [1, 'rgb(112, 180, 5)']
+                [0, '#0E2421'],
+                [1, '#337F75']
                 ]},{
             linearGradient: perShapeGradient,
             stops: [
-                [0, 'rgb(241, 244, 138)'],
-                [1, 'rgb(226, 233, 21)'],
+                [0, '#3F3F3F'],
+                [1, '#6E6F6F']
                 ]}, 
         ],
 	chart: {
